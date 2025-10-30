e6e4c265bf59d8d350d59cee338870675c28442d
Brian Park <park-brian@users.noreply.github.com>
2025-10-30 18:20:38 -0400

    update tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,19 +2,28 @@ import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloud
 import { describe, it, expect } from 'vitest';
 import worker from '../src';
 
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
-		const request = new Request('http://example.com');
-		// Create an empty context to pass to `worker.fetch()`.
+describe('Proxy worker', () => {
+	it('responds with example (unit style)', async () => {
+		const headers = { 'X-API-KEY': env.API_KEY };
+		const request = new Request('http://localhost/example.com', { headers });
 		const ctx = createExecutionContext();
 		const response = await worker.fetch(request, env, ctx);
+
 		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		const responseText = await response.text();
+		expect(responseText).toContain('Example');
 	});
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch('http://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+	it('responds with example (unit style)', async () => {
+		const headers = { 'X-API-KEY': env.API_KEY };
+		const request = new Request('http://localhost/https://jsonplaceholder.typicode.com/comments?postId=1', { headers });
+		const ctx = createExecutionContext();
+		const response = await worker.fetch(request, env, ctx);
+
+		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+		await waitOnExecutionContext(ctx);
+		const responseJson = await response.json();
+		expect(responseJson[0].postId).toBe(1);
 	});
 });
